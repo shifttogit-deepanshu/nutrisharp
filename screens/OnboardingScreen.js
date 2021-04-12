5ff09adaf086778d7903c2be277875613f0033c3
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-04-12 19:12:42 +0530

    init commit

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {View,StyleSheet,Text} from "react-native"
+
+
+const Onboarding = ()=>{
+    return (
+        <View>
+            <text>Onboarding screen</text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default Onboarding
