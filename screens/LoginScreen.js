52d325180036a4b012527a6671dda206a33a37d2
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-04-12 20:55:22 +0530

    added some onboarding screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {View,Text,StyleSheet} from "react-native"
+
+
+const LoginScreen = ()=>{
+    return (
+        <View><Text style={styles.head}>Login</Text></View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    head:{
+        fontSize:40
+    }
+})
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,17 +1,46 @@
 import React from "react"
-import {View,StyleSheet,Text} from "react-native"
+import {View,StyleSheet,Image,Text} from "react-native"
+import Onboarding from 'react-native-onboarding-swiper';
 
-
-const Onboarding = ()=>{
+const OnboardingScreen = ()=>{
     return (
-        <View>
-            <text>Onboarding screen</text>
-        </View>
+        <View style={styles.onboard}><Onboarding
+        pages={[
+          {
+            backgroundColor: '#fdeb93',
+            image: <Image style={styles.logo} source={require('../assets/images/ob1.png')} />,
+            title: 'Nutrition',
+            subtitle: 'Improving Nutrition focused Maternal New-born Child Health Practices Among Women',
+          },
+          {
+              backgroundColor: '#A6E4D0',
+              image: <Image style={styles.logo} source={require('../assets/images/ob2.png')} />,
+              title: 'Through a Life cycle Based Intervention',
+              subtitle: 'Reproductive Age Group (18-35 years)',
+            },
+            {
+              backgroundColor: 'white',
+              image: <Image style={styles.logo} source={require('../assets/images/logo.png')} />,
+              title: 'In Association',
+              subtitle: 'School Health Annual Report Programme',
+            }
+        ]}
+      /></View>
+        
     )
 }
 
 const styles = StyleSheet.create({
-    
+    logo:{
+        width:150,
+        height:150,
+        resizeMode: 'stretch'
+    },
+    onboard:{
+        flex:1,
+        width:"100%",
+        height:"100%"
+    }
 })
 
-export default Onboarding
+export default OnboardingScreen
